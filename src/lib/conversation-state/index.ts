a9a826ef80e139bbffd07c7f3519803c213ca0d9
kandamukeshkumar4 <kandamukeshkumar4@gmail.com>
2026-04-03 09:26:00 -0400

    feat(VET-717): add conversation state machine types

---
+++ src/lib/conversation-state/index.ts
@@ -0,0 +1,14 @@
+export {
+  CONVERSATION_STATE_VALUES,
+  QUESTION_STATE_VALUES,
+} from "./types";
+
+export type {
+  ConversationAnswerValue,
+  ConversationControlStateSnapshot,
+  ConversationState,
+  ConversationStateMachine,
+  QuestionState,
+  QuestionStateRecord,
+  StateTransition,
+} from "./types";
